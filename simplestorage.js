7f420a05db4f65f04a35e19689e10db199952277
DESKTOP-PDUPS8A\Samuel <ppk212@naver.com>
2018-08-17 00:48:10 +0900

    Chaintalk dApp Contract Code
    스마트 컨트랙트 코드 설명

---
+++ simplestorage.js
@@ -0,0 +1,70 @@
+document.write('<link rel="stylesheet" href="https://assets-cdn.github.com/assets/gist-embed-51733eabd598f6dd7e02254bb5e3dc72.css">')
+document.write('<div id=\"gist45773380\" class=\"gist\">\n
+                <div class=\"gist-file\">\n
+                <div class=\"gist-data\">\n
+                <div class=\"js-gist-file-update-container js-task-list-container file-box\">\n
+                <div id=\"file-simplestorage-sol\" class=\"file\">\n    \n\n
+                <div itemprop=\"text\" class=\"blob-wrapper data type-text\">\n
+                <table class=\"highlight tab-size js-file-line-container\" data-tab-size=\"8\">\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L1\" class=\"blob-num js-line-number\" data-line-number=\"1\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC1\" class=\"blob-code blob-code-inner js-file-line\">pragma solidity ^0.4.24;<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L2\" class=\"blob-num js-line-number\" data-line-number=\"2\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC2\" class=\"blob-code blob-code-inner js-file-line\">\n<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L3\" class=\"blob-num js-line-number\" data-line-number=\"3\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC3\" class=\"blob-code blob-code-inner js-file-line\">contract SimpleStorage {<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L4\" class=\"blob-num js-line-number\" data-line-number=\"4\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC4\" class=\"blob-code blob-code-inner js-file-line\">    uint storedData;<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L5\" class=\"blob-num js-line-number\" data-line-number=\"5\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC5\" class=\"blob-code blob-code-inner js-file-line\">\n<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L6\" class=\"blob-num js-line-number\" data-line-number=\"6\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC6\" class=\"blob-code blob-code-inner js-file-line\">    function set(uint x) {<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L7\" class=\"blob-num js-line-number\" data-line-number=\"7\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC7\" class=\"blob-code blob-code-inner js-file-line\">        storedData = x;<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L8\" class=\"blob-num js-line-number\" data-line-number=\"8\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC8\" class=\"blob-code blob-code-inner js-file-line\">    }<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L9\" class=\"blob-num js-line-number\" data-line-number=\"9\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC9\" class=\"blob-code blob-code-inner js-file-line\">\n<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L10\" class=\"blob-num js-line-number\" data-line-number=\"10\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC10\" class=\"blob-code blob-code-inner js-file-line\">    function get() constant returns (uint) {<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L11\" class=\"blob-num js-line-number\" data-line-number=\"11\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC11\" class=\"blob-code blob-code-inner js-file-line\">        return storedData;<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L12\" class=\"blob-num js-line-number\" data-line-number=\"12\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC12\" class=\"blob-code blob-code-inner js-file-line\">    }<\/td>\n
+                <\/tr>\n
+                <tr>\n
+                  <td id=\"file-simplestorage-sol-L13\" class=\"blob-num js-line-number\" data-line-number=\"13\"><\/td>\n
+                  <td id=\"file-simplestorage-sol-LC13\" class=\"blob-code blob-code-inner js-file-line\">}<\/td>\n
+                <\/tr>\n
+                <\/table>\n\n\n
+                <\/div>\n\n
+                <\/div>\n<\/div>\n\n
+                <\/div>\n
+                <div class=\"gist-meta\">\n
+                  <a href=\"https://gist.github.com/atomrigs/7c633570496b79623bed5d1286f93f3a/raw/6832c4af27645419a92ecba2d5afa080b07ffe6e/SimpleStorage.sol\" style=\"float:right\">view raw<\/a>\n
+                  <a href=\"https://gist.github.com/atomrigs/7c633570496b79623bed5d1286f93f3a#file-simplestorage-sol\">SimpleStorage.sol<\/a>\n        hosted with &#10084; by <a href=\"https://github.com\">GitHub<\/a>\n
+                <\/div>\n
+                <\/div>\n
+                <\/div>\n')
