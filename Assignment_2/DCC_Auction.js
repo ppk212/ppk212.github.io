568dfe23767dd935fc73b66337429e03b94f533a
Samuel Lee <ppk212@naver.com>
2018-11-23 16:43:19 +0900

    Assignment2_3rd
    Assignment2_3rd

--- Assignment_2/DCC_Auction.js
+++ Assignment_2/DCC_Auction.js
@@ -388,7 +388,7 @@ function buyTokens() {
 }
 
 function printClock() {
-    
+
     var clock = document.getElementById("clock");            // 출력할 장소 선택
     var currentDate = new Date();                                     // 현재시간
     var calendar = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate() // 현재 날짜
