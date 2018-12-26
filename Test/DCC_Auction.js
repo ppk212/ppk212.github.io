9f3634028c95ad886cbe3cb9d52d37e964ea7923
ppk212 <ppk212@naver.com>
2018-12-26 15:29:34 +0900

    Test6 모하메드
    Test6 모하메드

---
+++ Test/DCC_Auction.js
@@ -0,0 +1,346 @@
+let contractAddress = '0x4e29b7d45b162168cca760787d069d24f61586c8';
+let abi =
+[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "token_bid",
+				"type": "uint256"
+			}
+		],
+		"name": "Bid",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "buy",
+		"outputs": [
+			{
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_totalToken",
+				"type": "uint256"
+			},
+			{
+				"name": "_tokenPrice",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "balanceTokens",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "bid_value",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"name": "buyerReceived",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "buyers",
+		"outputs": [
+			{
+				"name": "buyerAddress",
+				"type": "address"
+			},
+			{
+				"name": "tokenBought",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getBalanceTokens",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getbid",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHighest",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getTokenBought",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getTokenPrice",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getTotalToken",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "highest_value",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "tokenPrice",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalToken",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+let auctionContract;
+let simpleAuction;
+let accountAddress;
+let currentEtherBalance;
+let currentTokenBalance;
+let tokenPrice;
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+  // Now you can start your app & access web3 freely:
+  startApp();
+});
+
+function startApp() {
+  auctionContract = web3.eth.contract(abi);
+  simpleAuction = auctionContract.at(contractAddress);
+  document.getElementById('contractAddr').innerHTML = getLink(contractAddress);
+
+  web3.eth.getAccounts(function(e,r){
+  document.getElementById('accountAddr').innerHTML = getLink(r[0]);
+  accountAddress = r[0];
+  getValue();
+  });
+}
+
+function getLink(addr) {
+  return '<a target="_blank" href=https://testnet.etherscan.io/address/' + addr + '>' + addr +'</a>';
+}
+
+function getValue() {
+  getEther();
+  getToken();
+  getTokenInfo();
+	gethigh();
+	getbid_value();
+}
+
+function gethigh(){
+	simpleAuction.getHighest(function(e,r){
+		document.getElementById('highest_iphone7').innerHTML = r.toString();
+	});
+}
+function getbid_value(){
+	simpleAuction.getbid(function(e,r){
+		document.getElementById('myself_iphone7').innerHTML = r.toString();
+	});
+}
+function getEther() {
+  web3.eth.getBalance(accountAddress, function(e,r){
+    document.getElementById('ethValue').innerHTML =web3.fromWei(r.toString()) + "ETH";
+  });
+}
+
+function getToken() {
+  simpleAuction.getTokenBought(function(e,r){
+    document.getElementById('tokenValue').innerHTML = r.toString();
+  });
+}
+
+function getTokenInfo() {
+
+  simpleAuction.getTokenPrice(function(e,r){
+  tokenPrice = parseFloat(web3.fromWei(r.toString()));
+    document.getElementById('token-cost').innerHTML = tokenPrice + "ETH";
+  });
+  web3.eth.getBalance(simpleAuction.address, function(e,v) {
+    document.getElementById('contract-balance').innerHTML = web3.fromWei(v.toString()) + "ETH";
+  });
+}
+
+
+function buyTokens() {
+  let tokensToBuy = $("#buy").val();
+  console.log(tokensToBuy);
+  console.log(tokenPrice);
+  let price = tokensToBuy * tokenPrice;
+  console.log(price);
+  $("#buy-msg").html("Purchase order has been submitted. Please wait.");
+
+  simpleAuction.buy({value: web3.toWei(price, 'ether'), from: web3.eth.accounts[0]}, function(v) {
+    web3.eth.getBalance(simpleAuction.address, function(e, r) {
+    $("#contract-balance").html(web3.fromWei(r.toString()) + " ETH");
+   });
+  });
+}
+
+function voteForProduct(i) {
+  let device_phone = $("#highest_iphone7").val();
+	if (typeof device_phone == 'undefined')
+		device_phone=0;
+  let mybid = $("#tb_iphone7").val();
+  simpleAuction.Bid(mybid, function (e, r){
+
+  });
+}
