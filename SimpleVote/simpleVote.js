488bac7a282012284d41239185c7fc91d818ec00
Lee Hyeonsu <ppk212@naver.com>
2019-08-31 21:23:05 +0900

    port 8080

--- SimpleVote/simpleVote.js
+++ SimpleVote/simpleVote.js
@@ -150,7 +150,7 @@ window.addEventListener('load', function() {
   } else {
     console.log('No web3? You should consider trying MetaMask!')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8080"));
   }
   // Now you can start your app & access web3 freely:
   startApp();
